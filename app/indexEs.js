dfde42028c0c8dd8805ad52d790e8571ae843f26
Ronni Egeriis Persson <ronni@egeriis.me>
2019-06-12 22:43:58 +0200

    Remove obsolete entry

--- app/indexEs.js
+++
@@ -1,7 +0,0 @@
-import { cube } from 'math';
-
-const numbers = [1,2,3,5,8];
-
-export default function() {
-  return numbers.map(n => cube(n));
-}
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: './index.js',
-    indexEs: './indexEs.js'
+    index: './index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
